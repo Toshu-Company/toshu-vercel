9762661017409a608bdedd781d80ed3171e6a4eb
Anonymous <>
2024-05-17 03:02:21 +0900

    fix

--- next.config.mjs
+++ next.config.mjs
@@ -13,10 +13,6 @@ const nextConfig = {
                         key: 'Access-Control-Allow-Methods',
                         value: 'GET, POST, PUT, DELETE, OPTIONS',
                     },
-                    {
-                        key: 'Access-Control-Allow-Headers',
-                        value: 'X-Requested-With, Content-Type, Accept, Origin, Authorization',
-                    },
                 ],
             },
         ];
