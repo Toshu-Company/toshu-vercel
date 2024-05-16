e4b148471f5e71986896cf5a36f7208dd29f5804
Anonymous <>
2024-05-17 01:23:05 +0900

    test

--- src/app/api/hitomi/detail/route.ts
+++ src/app/api/hitomi/detail/route.ts
@@ -3,6 +3,8 @@ import { NextRequest, NextResponse } from "next/server";
 
 export const runtime = "edge";
 
+export const unstable_allowDynamic = ["/src/libs/hitomi/unsafe.ts"];
+
 export async function GET(request: NextRequest) {
   return NextResponse.json(await HitomiAPI.getGG());
 }
