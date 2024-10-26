09c23b89ce7b0e7d822af4e78bc430edca8eb723
Anonymous <>
2024-10-26 20:32:18 +0900

    feat: get method mirror

--- src/app/api/yatv/mirror/route.ts
+++ src/app/api/yatv/mirror/route.ts
@@ -2,6 +2,34 @@ import { CDN } from "../utils";
 
 export const runtime = "edge";
 
+export async function GET(request: Request) {
+  const rawParams = request.url.split("?")[1];
+  const params = new URLSearchParams(rawParams);
+  if (!params.has("url")) {
+    return new Response("Missing URL parameter", {
+      status: 400,
+      statusText: "Bad Request",
+    });
+  }
+
+  const url = atob(params.get("url")!);
+  const res = await fetch(url, {
+    headers: {
+      Referer: CDN,
+    },
+  });
+
+  const blob = await res.blob();
+
+  return new Response(blob, {
+    status: 200,
+    statusText: "OK",
+    headers: {
+      "Content-Type": res.headers.get("Content-Type") || "video/mp4",
+    },
+  });
+}
+
 export async function POST(request: Request) {
   const body = await request.json();
 
