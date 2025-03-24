12e933c093fd8fd0744c8b74c98725e97231310b
Anonymous <>
2025-03-25 02:19:57 +0900

    fix: domain

--- src/libs/hitomi/const.ts
+++ src/libs/hitomi/const.ts
@@ -1,4 +1,4 @@
 export const compressed_nozomi_prefix = "n";
-export const domain = "hitomi.la";
+export const domain = "gold-usergeneratedcontent.net";
 export const max_node_size = 464;
 export const B = 16;
