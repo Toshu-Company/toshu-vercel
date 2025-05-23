5d5c2aff1fcc2a3a69ca7906a887009ed8ec2fdf
Anonymous <>
2025-05-23 18:22:22 +0900

    feat: add GET method to fetch video data from monsnode

---
+++ src/app/api/monsnode/route.ts
@@ -0,0 +1,62 @@
+import * as cheerio from "cheerio";
+import { NextRequest, NextResponse } from "next/server";
+
+export const runtime = "edge";
+
+enum VideoType {
+  LIVE_DL = 0,
+  DL_LIST = 1,
+}
+
+interface Video {
+  id?: string;
+  url?: string;
+  content?: string;
+  thumbnail?: string;
+}
+
+export async function GET(request: NextRequest) {
+  try {
+    const rawParams = request.url.split("?")[1];
+    const params = new URLSearchParams(rawParams);
+    const page = Number(params.get("page")) || 0;
+
+    const url = new URL("https://monsnode.com/");
+    url.searchParams.set("page", page.toString());
+
+    const res = await fetch(url, {
+      method: "GET",
+      cache: "no-store",
+    })
+      .then((res) => res.text())
+      .then((res) => cheerio.load(res))
+      .then(($) => {
+        const videos: Video[] = [];
+        $("div.listn").each((i, el) => {
+          const $el = $(el);
+          const video: Video = {
+            id: $el.attr("id"),
+            url: $el.find("a").attr("href"),
+            content: $el.find("img").attr("alt"),
+            thumbnail: $el.find("img").attr("src"),
+          };
+          videos.push(video);
+        });
+        return videos;
+      });
+
+    return NextResponse.json(res, {
+      headers: {
+        "Cache-Control": "public, max-age=60",
+        "CDN-Cache-Control": "public, s-maxage=600",
+        "Vercel-CDN-Cache-Control": "public, s-maxage=600",
+      },
+    });
+  } catch (error) {
+    console.error("Error fetching Twivideo data:", error);
+    return NextResponse.json(
+      { error: "Failed to fetch data" },
+      { status: 500 }
+    );
+  }
+}
